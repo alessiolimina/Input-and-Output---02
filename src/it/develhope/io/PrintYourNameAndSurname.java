2ec9c8c8d9854b7ad9b78610cd940dfac99bd009
Alessio Limina <alessio.limina@gmail.com>
2023-01-19 22:05:00 +0100

    Exercise: input-and-output-2

---
+++ src/it/develhope/io/PrintYourNameAndSurname.java
@@ -0,0 +1,12 @@
+package it.develhope.io;
+
+public class PrintYourNameAndSurname {
+
+    public static void main(String[] args) {
+
+        System.out.print("Alessio ");
+        System.out.print("Limina");
+
+    }
+
+}
